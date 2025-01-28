d015379d378db4ff2e70b67e3098a8b75d6f5f01
Pasante Sistemas <Pasante.Sistemas@interacables.com>
2025-01-28 14:07:55 -0400

    modificacion en semantica

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,4 @@
-import { Header } from "./components/header";
+import { Header } from "./components/Header";
 import { MainSection } from "./components/MainSection";
 import { Body } from "./components/Body";
 import { Footer } from "./components/Footer";
